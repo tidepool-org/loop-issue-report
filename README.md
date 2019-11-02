0fa6d32979b1bd1c7a9af9fc56c5bbd92bc1e647
russ <russ@tidepool.org>
2019-11-01 19:27:27 -0500

    initial install

---
+++ README.md
@@ -0,0 +1,12 @@
+### To recreate the Virtual Environment
+1. This environment was developed with Anaconda. You'll need to install [Miniconda](https://conda.io/miniconda.html) or [Anaconda](https://anaconda-installer.readthedocs.io/en/latest/) for your platform.
+2. In a terminal, navigate to the directory where the environment.yml 
+is located (likely the loop-issue-report folder).
+3. Run `conda env create`; this will download all of the package dependencies
+and install them in a virtual environment named loop-issue-report.  
+
+### To use the Virtual Environment
+In Bash run `source activate loop-issue-report`, or in the Anaconda Prompt
+run `conda activate loop-issue-report` to start the environment.
+
+Run `deactivate` to stop the environment.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info < (3, 7):
+    sys.exit("Sorry, Python < 3.7 is not supported")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version_string = "v0.0.1"
+
+setup(
+    name="loopIssueReport",
+    version=version_string,
+    author="Tidepool",
+    author_email="ed@tidepool.org",
+    description="Python implementation of the Loop issue report parsing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tidepool-org/PyLoopKit",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD-2-Clause',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.6',
+)
